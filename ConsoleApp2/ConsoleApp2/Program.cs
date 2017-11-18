b02cdd7385fb85378b07e8f4297680339b21f154
2068 <2068314880@qq.com>
2017-11-19 00:45:29 +0800

    C#

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using N1;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A oa = new A();
+            oa.Myls();
+        }
+    }
+}
+namespace N1
+{
+    class A
+    {
+        public void Myls()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = 300;
+            byte two = 200;
+            float f = 9.27f;
+            double dd = 9.20d;
+            bool b = true;
+            Console.WriteLine("noe={0}", one);
+            Console.WriteLine("two={0}", two);
+        }
+    }
+}
